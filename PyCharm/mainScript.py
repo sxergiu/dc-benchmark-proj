5ee1c652ecdc2f31734b808b329c4f79d526f86a
sxergiu <sergiu.varga@student.upt.ro>
2024-04-21 20:29:50 +0300

    init

---
+++ PyCharm/mainScript.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+def open_settings():
+    print("Settings button clicked")
+
+def exit_app():
+    print("Exit button clicked")
+    root.quit()
+
+def run_benchmarks():
+    print("Benchmarks button clicked")
+
+root = tk.Tk()
+root.title("Benchmarks of The Unnamed")
+
+
+#create buttons
+button_benchmarks = tk.Button(root, text="Benchmarks", command=run_benchmarks)
+button_benchmarks.pack(pady=5)
+
+button_settings = tk.Button(root, text="Settings", command=open_settings)
+button_settings.pack(pady=5)
+
+button_exit = tk.Button(root, text="Exit", command=exit_app)
+button_exit.pack(pady=5)
+
+root.mainloop()
