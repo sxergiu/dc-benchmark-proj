a7fd6b3c9a6ec6426923a488c66b978febf869af
crystisor <cristian.faur@student.upt.ro>
2024-05-13 22:08:56 +0300

    Solved some matrix bugs

--- PyCharm/Benchmarks/MatMult/randomMatrixGen.py
+++ PyCharm/Benchmarks/MatMult/randomMatrixGen.py
@@ -2,18 +2,19 @@ import numpy as np
 
 """ 2 matrices per file, also 2 multiplied per process  => powers of 2
 """
-matrix_size = (50, 50)    #size
+list_of_tuples_size = (25, 25)    #size
 num_pairs = 4          #numbers of tuples
 chunk_size = 2         #numbers of matrices multiplied per process
 
 
-def write_in_file(matrix):
-    with open("MatricesDB/test.txt", "a") as fp:
-        for row in matrix:
-            for num in row:
-                fp.write(str(num) + ' ')
-            fp.write('\n')
-        fp.write('\n')  # Add a newline between matrices
+def write_in_file(list_of_tuples):
+    with open("dc-benchmark-proj\\MatricesDB\\test.txt", "a") as fp:
+        for i in range(0,2):
+            matrix = list_of_tuples[0][i]
+            for x in range(list_of_tuples_size[0]):  #rows
+                for y in range(list_of_tuples_size[0]):   #columns
+                    fp.write(str(matrix[x][y]) + " ")
+                fp.write('\n')
 
 
 
@@ -23,8 +24,8 @@ def write_in_file(matrix):
 def generate_matrices():   
     array_of_mat = []
     for _ in range(num_pairs): 
-        matrix = [(np.random.rand(*matrix_size), np.random.rand(*matrix_size))]
-        array_of_mat.append(matrix)
-        write_in_file(matrix)
+        list_of_tuples = [(np.random.rand(*list_of_tuples_size), np.random.rand(*list_of_tuples_size))]
+        array_of_mat.append(list_of_tuples)
+        write_in_file(list_of_tuples)
     print("This is array of mat: " + '\n' + str(array_of_mat) + '\n')
     return array_of_mat
